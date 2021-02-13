9d34bd738cbb722fd54030e0080c957aa5ccbf07
Stefan Saraev <stefan@saraev.ca>
2021-02-13 14:13:52 +0200

    [update] stm32flash-0.6

--- packages/extra/stm32flash/package.mk
+++ packages/extra/stm32flash/package.mk
@@ -1,12 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="stm32flash"
-PKG_VERSION="0.5"
+PKG_VERSION="0.6"
 PKG_SITE="https://sourceforge.net/projects/stm32flash/"
 PKG_WATCH="http://sf.net/stm32flash/stm32flash-(\d\S*)\.(?:tgz|tbz|txz|(?:tar\.(?:gz|bz2|xz)))"
 PKG_URL="https://prdownloads.sourceforge.net/project/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
 
 MAKEFLAGS="$MAKEFLAGS PREFIX=/usr"
+
+post_unpack() {
+  rm -f $PKG_BUILD/configure
+}
 
 pre_configure_target() {
   LDFLAGS="$LDFLAGS -static"
