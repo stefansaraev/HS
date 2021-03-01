75d9b081ef9f57a8f1377789caae820521192884
Stefan Saraev <stefan@saraev.ca>
2021-03-01 19:09:19 +0200

    [update] e2fsprogs-1.46.2

--- packages/extra/e2fsprogs/package.mk
+++ packages/extra/e2fsprogs/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="e2fsprogs"
-PKG_VERSION="1.46.1"
+PKG_VERSION="1.46.2"
 PKG_SITE="https://github.com/tytso/e2fsprogs"
 PKG_WATCH="https://github.com/tytso/e2fsprogs/releases/ .*/archive/v([\d\.]*)\.tar\.gz"
 PKG_URL="https://github.com/tytso/e2fsprogs/archive/v$PKG_VERSION.tar.gz"
