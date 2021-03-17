ad18ad3f04c707c3296a2455ae051fed5c796be8
Stefan Saraev <stefan@saraev.ca>
2021-03-17 12:44:32 +0200

    [update] wireguard-tools-1.0.20210315

--- packages/extra/wireguard-tools/package.mk
+++ packages/extra/wireguard-tools/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="wireguard-tools"
-PKG_VERSION="1.0.20210223"
+PKG_VERSION="1.0.20210315"
 PKG_SITE="http://www.harding.motd.ca/wireguard-tools/"
 PKG_WATCH="https://git.zx2c4.com/wireguard-tools/ /wireguard-tools/snapshot/wireguard-tools-(.*)\.tar.xz"
 PKG_URL="https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-${PKG_VERSION}.tar.xz"
