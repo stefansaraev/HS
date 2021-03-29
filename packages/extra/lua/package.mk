937ff6ddc95b8326cec2d502f74dc8365109b427
Stefan Saraev <stefan@saraev.ca>
2021-03-29 15:58:30 +0300

    [update] lua-5.4.3

--- packages/extra/lua/package.mk
+++ packages/extra/lua/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="lua"
-PKG_VERSION="5.4.2"
+PKG_VERSION="5.4.3"
 PKG_SITE="http://www.lua.org/"
 PKG_WATCH="http://www.lua.org/ftp lua-([\d\.]*)\.tar\.gz"
 PKG_URL="http://www.lua.org/ftp/lua-$PKG_VERSION.tar.gz"
