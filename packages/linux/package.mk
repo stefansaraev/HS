830d7de88179fe8381f10da1642b2da316e13b2b
Stefan Saraev <stefan@saraev.ca>
2021-05-25 12:37:21 +0300

    [update] linux-cee9cfe368

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="linux"
-PKG_VERSION="31b07b274f"
+PKG_VERSION="cee9cfe368"
 PKG_SITE="https://github.com/raspberrypi/linux/tree/rpi-5.10.y"
 PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
