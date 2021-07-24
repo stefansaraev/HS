93c8f5fff657504e57f1d321b834ef08cd74246b
Stefan Saraev <stefan@saraev.ca>
2021-07-24 14:12:30 +0300

    [update] i2c-tools-4.3

--- packages/other/i2c-tools/package.mk
+++ packages/other/i2c-tools/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="i2c-tools"
-PKG_VERSION="4.2"
+PKG_VERSION="4.3"
 PKG_SITE="https://i2c.wiki.kernel.org/index.php/I2C_Tools"
 PKG_WATCH="https://mirrors.edge.kernel.org/pub/software/utils/i2c-tools/ i2c-tools-([\d\.]*)\.tar\.xz"
 PKG_URL="https://git.kernel.org/pub/scm/utils/i2c-tools/i2c-tools.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
