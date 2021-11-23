b9f0ab02f44e74fc2ff33843d54be884544c6323
Stefan Saraev <stefan@saraev.ca>
2021-11-24 00:08:16 +0200

    add: zerotier-one

---
+++ packages/extra/zerotier-one/package.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+PKG_NAME="zerotier-one"
+PKG_VERSION="7f4cc1a239"
+PKG_SITE="https://github.com/zerotier/ZeroTierOne/tree/master"
+PKG_URL="https://github.com/zerotier/ZeroTierOne/archive/$PKG_VERSION.tar.gz"
+
+post_unpack() {
+  rm -f $PKG_BUILD/CMakeLists.txt
+}
+
+pre_configure_target() {
+  export CXXFLAGS="$CXXFLAGS -D_GLIBCXX_USE_C99_STDIO=1 -D_GLIBCXX_USE_C99_STDLIB=1 -static"
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+  cp -PR zerotier-one $INSTALL/usr/bin
+  ln -sf zerotier-one $INSTALL/usr/bin/zerotier-idtool
+  ln -sf zerotier-one $INSTALL/usr/bin/zerotier-cli
+}
