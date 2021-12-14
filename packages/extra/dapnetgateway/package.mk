8946a626ef6a64c2a0b5ad53393112878860f8f6
Stefan Saraev <stefan@saraev.ca>
2021-12-14 20:44:05 +0200

    [update] dapnetgateway-631297ece7

--- packages/extra/dapnetgateway/package.mk
+++ packages/extra/dapnetgateway/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="dapnetgateway"
-PKG_VERSION="f8df3f24ad"
+PKG_VERSION="631297ece7"
 PKG_SITE="https://github.com/g4klx/DAPNETGateway/tree/master"
 PKG_URL="https://github.com/g4klx/DAPNETGateway/archive/$PKG_VERSION.tar.gz"
 
