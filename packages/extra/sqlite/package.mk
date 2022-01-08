fdad5d5655f04f911b432549ecc3f11ddab0548e
Stefan Saraev <stefan@saraev.ca>
2022-01-08 17:33:01 +0200

    [update] sqlite-3370200

--- packages/extra/sqlite/package.mk
+++ packages/extra/sqlite/package.mk
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="sqlite"
-PKG_VERSION="3370100"
+PKG_VERSION="3370200"
 PKG_SITE="https://www.sqlite.org/"
 PKG_WATCH="https://distfiles.macports.org/sqlite3/ sqlite-autoconf-(.*)\.tar\.gz"
-PKG_URL="https://www.sqlite.org/2021/$PKG_NAME-autoconf-$PKG_VERSION.tar.gz"
+PKG_URL="https://www.sqlite.org/2022/$PKG_NAME-autoconf-$PKG_VERSION.tar.gz"
 
 PKG_DEPENDS_TARGET="readline"
 
