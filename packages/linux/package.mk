4b58a9b4c1d4f112ee2f9d20d4fdf3a138d88e05
Stefan Saraev <stefan@saraev.ca>
2022-01-08 17:40:51 +0200

    [update] linux-bee5000597

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="linux"
-PKG_VERSION="1d6957db7f"
+PKG_VERSION="bee5000597"
 PKG_SITE="https://github.com/raspberrypi/linux/tree/rpi-5.10.y"
 PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
