70f067781aeea36d0d61eeaad70a27e65322a4dd
Stefan Saraev <stefan@saraev.ca>
2022-02-25 22:18:11 +0200

    [remove] nftables

--- packages/virtual/extra/package.mk
+++ packages/virtual/extra/package.mk
@@ -3,7 +3,7 @@
 PKG_NAME="extra"
 PKG_DEPENDS_TARGET="
   autossh curl e2fsprogs file i2c-utils libressl lua mosquitto parted php rpi-userland rsync screen sqlite vim
-  bwm-ng dte ethtool iw iptables mtr nftables ngrep tcpdump wireguard-tools zerotier-one
+  bwm-ng dte ethtool iw iptables mtr ngrep tcpdump wireguard-tools zerotier-one
   gdb strace
   mmdvmcal displayserver dapnetgateway dmrhost nextiondriver
   oledctl stm32flash
