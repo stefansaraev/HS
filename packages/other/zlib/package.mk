12a5d6d235ef47107625b9f9e5e92fc9779064f3
Stefan Saraev <stefan@saraev.ca>
2022-04-15 23:58:24 +0300

    [update] zlib-1.2.12

--- packages/other/zlib/package.mk
+++ packages/other/zlib/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="zlib"
-PKG_VERSION="1.2.11"
+PKG_VERSION="1.2.12"
 PKG_SITE="http://www.zlib.net"
 PKG_WATCH="http://www.zlib.net/ zlib-([\d\.]*)\.tar\.gz"
 PKG_URL="http://zlib.net/$PKG_NAME-$PKG_VERSION.tar.xz"
