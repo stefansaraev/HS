2b298ca8c40668b1dcef4e3ed1c796a0e8af14b2
Stefan Saraev <stefan@saraev.ca>
2022-05-06 15:45:25 +0300

    [update] oniguruma-6.9.8

--- packages/extra/oniguruma/package.mk
+++ packages/extra/oniguruma/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="oniguruma"
-PKG_VERSION="6.9.7.1"
+PKG_VERSION="6.9.8"
 PKG_SITE="https://github.com/kkos/oniguruma"
 PKG_WATCH="https://github.com/kkos/oniguruma/releases/ .*/archive/refs/tags/v([\d\.]*)\.tar\.gz"
 PKG_URL="https://github.com/kkos/oniguruma/archive/v$PKG_VERSION.tar.gz"
