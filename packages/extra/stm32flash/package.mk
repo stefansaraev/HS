7932a2f9cc29d582e0eb5f1563565d91f24b365f
Stefan Saraev <stefan@saraev.ca>
2022-05-12 19:46:06 +0300

    stm32flash: update PKG_WATCH

--- packages/extra/stm32flash/package.mk
+++ packages/extra/stm32flash/package.mk
@@ -3,7 +3,7 @@
 PKG_NAME="stm32flash"
 PKG_VERSION="0.7"
 PKG_SITE="https://sourceforge.net/projects/stm32flash/"
-PKG_WATCH="http://sf.net/stm32flash/stm32flash-(\d\S*)\.(?:tgz|tbz|txz|(?:tar\.(?:gz|bz2|xz)))"
+PKG_WATCH="https://sourceforge.net/projects/stm32flash/files/ .*/stm32flash-([\d\.]*)\.tar\.gz/download"
 PKG_URL="https://prdownloads.sourceforge.net/project/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
 
 MAKEFLAGS="$MAKEFLAGS PREFIX=/usr"
