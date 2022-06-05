8358632beea4bf9c08781b4ec1bd0703284135f1
Stefan Saraev <stefan@saraev.ca>
2022-06-05 20:18:22 +0300

    [update] dmrhost-4f7f626c67

--- packages/base/dmrhost/package.mk
+++ packages/base/dmrhost/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="dmrhost"
-PKG_VERSION="982aaee258"
+PKG_VERSION="4f7f626c67"
 PKG_SITE="https://github.com/stefansaraev/DMRHost/tree/dev"
 PKG_URL="https://github.com/stefansaraev/DMRHost/archive/$PKG_VERSION.tar.gz"
 
