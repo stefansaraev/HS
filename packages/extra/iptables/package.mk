bc4c1c86fa03466378ed5047155e9d8329903f37
Stefan Saraev <stefan@saraev.ca>
2022-06-17 22:13:19 +0300

    [update] iptables-1.8.8

--- packages/extra/iptables/package.mk
+++ packages/extra/iptables/package.mk
@@ -1,11 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="iptables"
-PKG_VERSION="1.8.7"
+PKG_VERSION="1.8.8"
 PKG_SITE="http://www.netfilter.org/"
 PKG_WATCH="https://netfilter.org/projects/iptables/downloads.html .*/iptables-(.*)\.tar.bz2"
 PKG_URL="http://www.netfilter.org/projects/iptables/files/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="libmnl libnftnl"
+
+pre_configure_target() {
+  rm -rf $PKG_BUILD_SUBDIR
+  CFLAGS="$CFLAGS -D__UAPI_DEF_ETHHDR=0"
+}
 
 PKG_CONFIGURE_OPTS_TARGET="
   --enable-static --disable-shared
