7f63987f922718746ec0c7f4f1f875191bd8f7c4
Stefan Saraev <stefan@saraev.ca>
2022-07-03 16:36:54 +0300

    [update] zerotier-one-651f45fe29

--- packages/extra/zerotier-one/package.mk
+++ packages/extra/zerotier-one/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="zerotier-one"
-PKG_VERSION="b4cec0b4a8"
+PKG_VERSION="651f45fe29"
 PKG_SITE="https://github.com/zerotier/ZeroTierOne/tree/master"
 PKG_URL="https://github.com/zerotier/ZeroTierOne/archive/$PKG_VERSION.tar.gz"
 
