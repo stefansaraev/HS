89b128970017f22a95962e326fc613d8cfea1fa2
Stefan Saraev <stefan@saraev.ca>
2022-08-12 19:18:09 +0300

    [update] libnftnl-1.2.3

--- packages/extra/libnftnl/package.mk
+++ packages/extra/libnftnl/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="libnftnl"
-PKG_VERSION="1.2.2"
+PKG_VERSION="1.2.3"
 PKG_SITE="http://netfilter.org/projects/libnftnl"
 PKG_WATCH="https://netfilter.org/projects/libnftnl/downloads.html .*/libnftnl-(.*)\.tar.bz2"
 PKG_URL="http://netfilter.org/projects/libnftnl/files/$PKG_NAME-$PKG_VERSION.tar.bz2"
