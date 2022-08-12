a183250eaa7eba211cc0c1b8d57b04b2807cb9a9
Stefan Saraev <stefan@saraev.ca>
2022-08-12 19:35:09 +0300

    [update] bcm2835-bootloader-e1e3dc004e

--- packages/base/bcm2835-bootloader/package.mk
+++ packages/base/bcm2835-bootloader/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="89095bb812"
+PKG_VERSION="e1e3dc004e"
 PKG_SITE="https://github.com/raspberrypi/firmware/tree/stable"
 PKG_URL="https://github.com/raspberrypi/firmware/archive/$PKG_VERSION.tar.gz"
 
