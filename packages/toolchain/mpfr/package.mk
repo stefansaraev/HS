839c231c1ca78709a422b5565d78ac4555124fb5
Stefan Saraev <stefan@saraev.ca>
2022-12-09 19:10:51 +0200

    [update] mpfr-4.1.1

--- packages/toolchain/mpfr/package.mk
+++ packages/toolchain/mpfr/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="mpfr"
-PKG_VERSION="4.1.0"
+PKG_VERSION="4.1.1"
 PKG_SITE="http://www.mpfr.org/"
 PKG_WATCH="http://ftp.gnu.org/gnu/mpfr/ mpfr-([\d\.]*)\.tar\.xz"
 PKG_URL="http://ftp.gnu.org/gnu/mpfr/$PKG_NAME-$PKG_VERSION.tar.xz"
