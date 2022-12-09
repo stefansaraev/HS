4445c633f3c75cac6416686edca60bb1cd78292c
Stefan Saraev <stefan@saraev.ca>
2022-12-09 19:11:16 +0200

    [update] linux-rpi4-912e47c9ce

--- packages/linux-rpi4/package.mk
+++ packages/linux-rpi4/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="linux-rpi4"
-PKG_VERSION="5645d10583"
+PKG_VERSION="912e47c9ce"
 PKG_SITE="https://github.com/raspberrypi/linux/tree/rpi-5.15.y"
 PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
