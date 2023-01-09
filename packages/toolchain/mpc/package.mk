0258dfff2c69defb77e398bd8cb50637c00be03f
Stefan Saraev <stefan@saraev.ca>
2023-01-09 20:26:00 +0200

    [update] mpc-1.3.1

--- packages/toolchain/mpc/package.mk
+++ packages/toolchain/mpc/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="mpc"
-PKG_VERSION="1.2.1"
+PKG_VERSION="1.3.1"
 PKG_SITE="http://www.multiprecision.org"
 PKG_WATCH="http://ftp.gnu.org/gnu/mpc/ mpc-([\d\.]*)\.tar\.gz"
 PKG_URL="http://ftp.gnu.org/gnu/mpc/$PKG_NAME-$PKG_VERSION.tar.gz"
