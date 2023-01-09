4b23a57722457b931bce9cfb4368d027d658e3b7
Stefan Saraev <stefan@saraev.ca>
2023-01-09 20:25:47 +0200

    [update] tcpdump-4.99.2

--- packages/extra/tcpdump/package.mk
+++ packages/extra/tcpdump/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="tcpdump"
-PKG_VERSION="4.99.1"
+PKG_VERSION="4.99.2"
 PKG_SITE="http://www.tcpdump.org/"
 PKG_WATCH="http://www.tcpdump.org/release/ tcpdump-([\d\.]*)\.tar\.gz"
 PKG_URL="http://www.tcpdump.org/release/$PKG_NAME-$PKG_VERSION.tar.gz"
