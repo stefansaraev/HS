3724da314baa8fda9b54bd85cae9053d907efe0e
Stefan Saraev <stefan@saraev.ca>
2023-01-09 22:05:49 +0200

    remove rngd

--- packages/virtual/base/package.mk
+++ packages/virtual/base/package.mk
@@ -2,7 +2,7 @@
 
 PKG_NAME="base"
 PKG_DEPENDS_TARGET="
-  toolchain musl initramfs linux linux-rpi4 busybox nano dropbear rngd
+  toolchain musl initramfs linux linux-rpi4 busybox nano dropbear
   bcm2835-bootloader wpa_supplicant
   opkg
   dmrhost
