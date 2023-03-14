dc17d6e95757d54770037ea1aa141bfd50d71327
Stefan Saraev <stefan@saraev.ca>
2023-03-14 19:26:39 +0200

    [update] zerotier-one-1.10.5

--- packages/extra/zerotier-one/package.mk
+++ packages/extra/zerotier-one/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="zerotier-one"
-PKG_VERSION="1.10.2"
-PKG_SITE="https://github.com/zerotier/ZeroTierOne/tree/master"
+PKG_VERSION="1.10.5"
+PKG_SITE="https://github.com/zerotier/ZeroTierOne"
 PKG_WATCH="https://github.com/zerotier/ZeroTierOne/tags/ .*/([\d\.]*)\.tar\.gz"
 PKG_URL="https://github.com/zerotier/ZeroTierOne/archive/refs/tags/$PKG_VERSION.tar.gz"
 
