46b0cdd7d7218438bd50f2bab7db12240a582dcf
Stefan Saraev <stefan@saraev.ca>
2023-04-26 13:40:03 +0300

    [update] rpi-userland-cc1ca18fb0

--- packages/extra/rpi-userland/package.mk
+++ packages/extra/rpi-userland/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="rpi-userland"
-PKG_VERSION="9d5250fd98"
+PKG_VERSION="cc1ca18fb0"
 PKG_SITE="https://github.com/raspberrypi/userland/tree/master"
 PKG_URL="https://github.com/raspberrypi/userland/archive/$PKG_VERSION.tar.gz"
 
