814353571f673efa68f9a299cef9838fe3a5e551
Stefan Saraev <stefan@saraev.ca>
2023-07-26 21:34:32 +0300

    [update] linux-1c16140c15

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="linux"
-PKG_VERSION="d4c3133378"
+PKG_VERSION="1c16140c15"
 PKG_SITE="https://github.com/raspberrypi/linux/tree/rpi-6.1.y"
 PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
