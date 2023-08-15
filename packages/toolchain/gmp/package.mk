a525b19b6e900e99d212e5400907fd0bee0bb65f
Stefan Saraev <stefan@saraev.ca>
2023-08-15 13:23:50 +0300

    [update] gmp-6.3.0

--- packages/toolchain/gmp/package.mk
+++ packages/toolchain/gmp/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="gmp"
-PKG_VERSION="6.2.1"
+PKG_VERSION="6.3.0"
 PKG_SITE="http://gmplib.org/"
 PKG_WATCH="https://gmplib.org/download/gmp/ gmp-([\d\.]*)\.tar\.xz"
 PKG_URL="https://gmplib.org/download/gmp/$PKG_NAME-$PKG_VERSION.tar.xz"
