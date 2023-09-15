0c76de2d79a627c9ee722244c88af2cc0670e0d4
Stefan Saraev <stefan@saraev.ca>
2023-09-15 15:21:35 +0300

    [update] linux-rpi4-e079555a4c

--- packages/linux-rpi4/package.mk
+++ packages/linux-rpi4/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="linux-rpi4"
-PKG_VERSION="19a1b03529"
+PKG_VERSION="e079555a4c"
 PKG_SITE="https://github.com/raspberrypi/linux/tree/rpi-6.1.y"
 PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
