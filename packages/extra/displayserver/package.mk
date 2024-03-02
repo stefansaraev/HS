26319cdd49eab1a6b2b8c759dd1321feb0e9f5d9
Stefan Saraev <stefan@saraev.ca>
2024-03-02 16:06:34 +0200

    [update] displayserver-5dc9030e9c

--- packages/extra/displayserver/package.mk
+++ packages/extra/displayserver/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="displayserver"
-PKG_VERSION="f97d172f9a"
+PKG_VERSION="5dc9030e9c"
 PKG_SITE="https://github.com/stefansaraev/DisplayServer/tree/master"
 PKG_URL="https://github.com/stefansaraev/DisplayServer/archive/$PKG_VERSION.tar.gz"
 
