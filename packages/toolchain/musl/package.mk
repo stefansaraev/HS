a366f376c1e9cb4beea24786b065a0f60ebf45e9
Stefan Saraev <stefan@saraev.ca>
2024-03-02 16:05:20 +0200

    [update] musl-1.2.5

--- packages/toolchain/musl/package.mk
+++ packages/toolchain/musl/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="musl"
-PKG_VERSION="1.2.4"
+PKG_VERSION="1.2.5"
 PKG_SITE="https://www.musl-libc.org/"
 PKG_WATCH="https://www.musl-libc.org/download.html .*/musl-(.*)\.tar\.gz"
 PKG_URL="https://www.musl-libc.org/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
