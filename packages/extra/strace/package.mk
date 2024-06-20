19c55abf051dd0378e7c6de30adfdd43fda61f99
Stefan Saraev <stefan@saraev.ca>
2024-06-20 15:06:33 +0000

    [update] strace-6.9

--- packages/extra/strace/package.mk
+++ packages/extra/strace/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="strace"
-PKG_VERSION="6.7"
+PKG_VERSION="6.9"
 PKG_SITE="https://github.com/strace/strace"
 PKG_WATCH="https://github.com/strace/strace/tags/ .*/v([\d\.]*)\.tar\.gz"
 PKG_URL="https://github.com/strace/strace/releases/download/v$PKG_VERSION/strace-$PKG_VERSION.tar.xz"
