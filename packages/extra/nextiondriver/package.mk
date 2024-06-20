5f37b76a48dd46fbe62dbd03325e802be3136451
Stefan Saraev <stefan@saraev.ca>
2024-06-20 15:07:26 +0000

    [update] nextiondriver-e2cbd4293f

--- packages/extra/nextiondriver/package.mk
+++ packages/extra/nextiondriver/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="nextiondriver"
-PKG_VERSION="17a6b95bfb"
+PKG_VERSION="e2cbd4293f"
 PKG_SITE="https://github.com/on7lds/NextionDriver/tree/master"
 PKG_URL="https://github.com/on7lds/NextionDriver/archive/$PKG_VERSION.tar.gz"
 
