a42bb7fc0b102680b4b942c5288eaa2c251c4958
Stefan Saraev <stefan@saraev.ca>
2024-07-24 19:42:33 +0000

    rpi-userland: remove

--- packages/virtual/extra/package.mk
+++ packages/virtual/extra/package.mk
@@ -2,7 +2,7 @@
 
 PKG_NAME="extra"
 PKG_DEPENDS_TARGET="
-  curl e2fsprogs file i2c-utils libressl lua mosquitto parted php rpi-userland rsync screen sqlite vim
+  curl e2fsprogs file i2c-utils libressl lua mosquitto parted php rsync screen sqlite vim
   bwm-ng dte ethtool iw iptables mtr ngrep tcpdump wireguard-tools zerotier-one
   gdb strace
   mmdvmcal displayserver dapnetgateway dmrhost nextiondriver
