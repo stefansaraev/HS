93483a7506f90b54254ff6152d8b8d292902c5e2
Stefan Saraev <stefan@saraev.ca>
2024-07-24 19:45:03 +0000

    dte: remove

--- packages/virtual/extra/package.mk
+++ packages/virtual/extra/package.mk
@@ -3,7 +3,7 @@
 PKG_NAME="extra"
 PKG_DEPENDS_TARGET="
   curl e2fsprogs file i2c-utils libressl lua mosquitto parted php rsync screen sqlite vim
-  bwm-ng dte ethtool iw iptables mtr ngrep tcpdump wireguard-tools zerotier-one
+  bwm-ng ethtool iw iptables mtr ngrep tcpdump wireguard-tools zerotier-one
   gdb strace
   mmdvmcal displayserver dapnetgateway dmrhost nextiondriver
   oledctl stm32flash
