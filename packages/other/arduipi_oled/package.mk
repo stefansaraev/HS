44e982da769e462eeaa34263f0f61e82648c18a8
Stefan Saraev <stefan@saraev.ca>
2024-08-09 18:06:33 +0000

    arduipi_oled: workaround gcc14 build

--- packages/other/arduipi_oled/package.mk
+++ packages/other/arduipi_oled/package.mk
@@ -4,3 +4,7 @@ PKG_NAME="arduipi_oled"
 PKG_VERSION="4119148b05"
 PKG_SITE="https://github.com/hallard/ArduiPi_OLED/tree/master"
 PKG_URL="https://github.com/hallard/ArduiPi_OLED/archive/$PKG_VERSION.tar.gz"
+
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -Wno-implicit-function-declaration"
+}
