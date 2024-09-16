19dd0bfbf018214d6d2ffe4c4a233c8a1bb5db3e
Stefan Saraev <stefan@saraev.ca>
2024-09-16 19:02:21 +0000

    [update] libarchive-3.7.5

--- packages/other/libarchive/package.mk
+++ packages/other/libarchive/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="libarchive"
-PKG_VERSION="3.7.4"
+PKG_VERSION="3.7.5"
 PKG_SITE="https://www.libarchive.org"
 PKG_WATCH="https://www.libarchive.org/downloads/ .*/libarchive-(.*)\.tar.gz"
 PKG_URL="https://www.libarchive.org/downloads/$PKG_NAME-$PKG_VERSION.tar.gz"
