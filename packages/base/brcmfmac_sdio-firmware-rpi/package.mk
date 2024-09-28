75139c95720c85feb4ecc7de987623b7b632c956
Stefan Saraev <stefan@saraev.ca>
2024-09-28 15:07:49 +0000

    [update] brcmfmac_sdio-firmware-rpi-4b356e134e

--- packages/base/brcmfmac_sdio-firmware-rpi/package.mk
+++ packages/base/brcmfmac_sdio-firmware-rpi/package.mk
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="brcmfmac_sdio-firmware-rpi"
-PKG_VERSION="39f45e47c4"
-PKG_SITE="https://github.com/LibreELEC/brcmfmac_sdio-firmware-rpi"
-PKG_URL="https://github.com/LibreELEC/brcmfmac_sdio-firmware-rpi/archive/$PKG_VERSION.tar.gz"
+PKG_VERSION="4b356e134e"
+PKG_SITE="https://github.com/RPi-Distro/firmware-nonfree/tree/bookworm"
+PKG_URL="https://github.com/RPi-Distro/firmware-nonfree/archive/$PKG_VERSION.tar.gz"
 PKG_NEED_REBUILD="initramfs"
 
 make_init() {
@@ -12,6 +12,15 @@ make_init() {
 
 makeinstall_init() {
   mkdir -p $INSTALL/lib/firmware/brcm
-  cp -PR $PKG_BUILD/firmware/brcm/brcmfmac43430-sdio.* $INSTALL/lib/firmware/brcm
-  cp -PR $PKG_BUILD/firmware/brcm/brcmfmac43455-sdio.* $INSTALL/lib/firmware/brcm
+  cp -i $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43430-sdio.bin $INSTALL/lib/firmware/brcm
+  cp -i $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43430-sdio.txt $INSTALL/lib/firmware/brcm
+
+  cp -i $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43436-sdio.bin $INSTALL/lib/firmware/brcm
+  cp -i $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43436-sdio.txt $INSTALL/lib/firmware/brcm
+  cp -i $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43436s-sdio.bin $INSTALL/lib/firmware/brcm
+  cp -i $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43436s-sdio.txt $INSTALL/lib/firmware/brcm
+
+  ln -sf ../cypress/cyfmac43455-sdio-minimal.bin $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin
+  cp -i $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin $INSTALL/lib/firmware/brcm
+  cp -i $PKG_BUILD/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt $INSTALL/lib/firmware/brcm
 }
--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -38,6 +38,7 @@ makeinstall_target() {
   cp arch/arm/boot/dts/broadcom/bcm2710-rpi-2-b.dtb $PKG_BUILD/.boot
   cp arch/arm/boot/dts/broadcom/bcm2710-rpi-3-b.dtb $PKG_BUILD/.boot
   cp arch/arm/boot/dts/broadcom/bcm2710-rpi-3-b-plus.dtb $PKG_BUILD/.boot
+  cp arch/arm/boot/dts/broadcom/bcm2710-rpi-zero-2-w.dtb $PKG_BUILD/.boot
 
   mkdir -p $PKG_BUILD/.boot/overlays
   cp arch/arm/boot/dts/overlays/disable-bt.dtbo $PKG_BUILD/.boot/overlays
