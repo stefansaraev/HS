15761a6826ad947d1b204dea5d7aa89cccadce02
Stefan Saraev <stefan@saraev.ca>
2025-01-11 18:48:46 +0200

    netbsd-curses: static bins

--- packages/other/netbsd-curses/package.mk
+++ packages/other/netbsd-curses/package.mk
@@ -7,9 +7,9 @@ PKG_WATCH="https://github.com/sabotage-linux/netbsd-curses/tags/ .*/v([\d\.]*)\.
 PKG_URL="https://github.com/sabotage-linux/netbsd-curses/archive/v$PKG_VERSION.tar.gz"
 
 make_target() {
-  make HOSTCC="$HOST_CC" PREFIX=/usr all-static
+  make HOSTCC="$HOST_CC" LDFLAGS="$LDFLAGS -static" STATIC_BINS=1 PREFIX=/usr all-static
 }
 
 makeinstall_target() {
-  make HOSTCC="$HOST_CC" PREFIX=$SYSROOT_PREFIX/usr install-static
+  make HOSTCC="$HOST_CC" LDFLAGS="$LDFLAGS -static" STATIC_BINS=1 PREFIX=$SYSROOT_PREFIX/usr install-static
 }
